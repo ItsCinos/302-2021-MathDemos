c1e7a8cad44bb0bc509a9884c3673ae2a56cde37
Tom H <darksonic3211@gmail.com>
2021-01-15 17:54:30 -0500

    Fixed Lab 1
    No longer appears to have any compile errors. Made a mess of scripts trying to fix it and I cleaned it up a bit. This should work.

--- AnimDemos/Assets/Scripts/LerpDemo.cs
+++ AnimDemos/Assets/Scripts/LerpDemo.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lerp : MonoBehaviour
+public class LerpDemo : MonoBehaviour
 {
 
     public GameObject startObject;
     public GameObject endObject;
 
     [Range(-1, 2)] public float percent = 0;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = AnimMath.Lerp(
-            startObject.transform.position, 
+            startObject.transform.position,
             endObject.transform.position,
             percent
             );
